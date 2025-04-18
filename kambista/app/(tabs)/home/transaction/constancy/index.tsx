e1af44182666ab38d9fe4b67b713d2dc7b336b2c
Edu-rz <edurz12345@gmail.com>
2025-04-17 21:39:39 -0500

    feat: transaction-constancy view

--- kambista/app/(tabs)/home/transaction/constancy/index.tsx
+++ kambista/app/(tabs)/home/transaction/constancy/index.tsx
@@ -1,33 +1,56 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Image, Text, ScrollView } from 'react-native';
+import { View, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { router } from 'expo-router';
+import { TransactionStepper } from '@/constants/Steppers';
+import Stepper from '@/components/Stepper';
+import Header from '@/components/Header';
+import Images from '@/constants/Images';
 
 const History = () => {
+  const navigateToHome = () => {
+    router.replace('/(tabs)/home');
+  };
+
   return (
     <SafeAreaView className="min-h-screen">
       <ScrollView className="px-6 py-2">
-        <View className="flex items-center justify-center mt-14">
-          {false ? (
-            <Image
-              className={`aspect-square bg-primary h-28 rounded-full`}
-              source={{
-                uri: undefined, // professional.foto,
-              }}
-            />
-          ) : (
-            <View className={`aspect-square bg-primary items-center justify-center h-28 rounded-full`}>
-              <Text className="text-5xl text-[#434343] font-qregular">{'Facundo'.charAt(0)}</Text>
-            </View>
-          )}
-          <View className="my-1" />
+        <View className="my-6" />
 
-          <Text className="font-psemibold text-xl font-qsemibold text-gray-800">Facundo Ramón Z.</Text>
-          <Text className="font-qmedium text-base text-gray-500">facundo@gmail.com</Text>
+        <View className="bg-white rounded-xl flex flex-column px-8 py-4">
+          <View className="justify-center items-center">
+            <Image source={Images.Logo} className="w-48" resizeMode="contain" />
+          </View>
+
+          <Text className="font-mbold text-2xl self-center">¡Constancia enviada!</Text>
+
+          <View className="flex-1 h-px bg-gray-300 my-2" />
+
+          <Text className="font-mmedium text-gray-800">Código Kambista</Text>
+          <Text className="font-mbold text-lg mb-2">kmf20ttff</Text>
+
+          <Text className="font-mmedium text-sm text-gray-800 mb-2">*Usa tu código para dar seguimiento a la operación</Text>
+
+          <Text className="font-mmedium text-sm text-gray-800">Monto a recibir</Text>
+          <Text className="font-mbold text-lg mb-2">S/. 343.00</Text>
+
+          <Text className="font-mmedium text-sm text-gray-800">Tiempo estimado de espera</Text>
+          <Text className="font-mbold text-lg mb-2">20h 15m</Text>
+          <View className="my-2" />
         </View>
 
+        <TouchableOpacity className="justify-center items-center">
+          <Image source={Images.Logo} className="w-48" resizeMode="contain" />
+        </TouchableOpacity>
+
+        <Text className="font-mregular self-center">Verificaremos tu operación. Puedes ver su estado en "Mis Operaciones"</Text>
         <View className="my-4" />
 
-        <View className="my-12" />
+        <TouchableOpacity onPress={navigateToHome} className="w-full py-5 rounded-xl bg-primary">
+          <Text className="text-center text-lg font-msemibold">VOLVER A INICIO</Text>
+        </TouchableOpacity>
+
+        <View className="my-16" />
       </ScrollView>
     </SafeAreaView>
   );
